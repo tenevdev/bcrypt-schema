c6f09387df0b857986f9b9eeeaa4bbf7f1808adb
Tencho Tenev <tenev.dev@outlook.com>
2015-02-07 16:51:14 +0200

    Update tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,13 +1,29 @@
 var bcryptSchema = require('..'),
     bcrypt = require('bcrypt'),
     sinon = require('sinon'),
-    compare, genSalt, hash
+    passingValue = 'passing-value',
+    failingValue = 'failing-value',
+    errorValue = 'error-value',
+    hashValue = 'hash-value',
+    hashField = 'hash-field',
+    saltValue = 'salt-value',
+    compare, passingCompare, failingCompare, errorCompare,
+    genSalt, passingGenSalt,
+    hash, passingHash, errorHash
 
 describe('bcrypt-schema', function() {
     before('mock bcrypt', function() {
-        compare = sinon.stub(bcrypt, 'compare').yields(null, true)
-        genSalt = sinon.stub(bcrypt, 'genSalt').yields(null, 'test-salt')
-        hash = sinon.stub(bcrypt, 'hash').yields(null, 'test-hash')
+        compare = sinon.stub(bcrypt, 'compare')
+        passingCompare = compare.withArgs(passingValue, hashValue).yields(null, true)
+        failingCompare = compare.withArgs(failingValue, hashValue).yields(null, false)
+        errorCompare = compare.withArgs(errorValue, hashValue).yields('error', undefined)
+
+        genSalt = sinon.stub(bcrypt, 'genSalt')
+        passingGenSalt = genSalt.withArgs(10).yields(null, saltValue)
+
+        hash = sinon.stub(bcrypt, 'hash')
+        passingHash = hash.withArgs(passingValue, saltValue).yields(null, hashValue)
+        errorHash = hash.withArgs(errorValue, saltValue).yields('error')
     })
     after('restore bcrypt', function() {
         bcrypt.compare.restore()
@@ -15,31 +31,60 @@ describe('bcrypt-schema', function() {
         bcrypt.hash.restore()
     })
     describe('verify(value, hash, next)', function() {
-        beforeEach('call verify', function(done) {
-            var value = 'test-value',
-                hash = 'test-hash'
-            bcryptSchema.verify(value, hash, function() {
-                done()
+        describe('when comparison succeeds', function() {
+            it('should call next with no error and true', function(done) {
+                bcryptSchema.verify(passingValue, hashValue, function(err, isVerified) {
+                    expect(err).to.not.exist
+                    expect(isVerified).to.equal(true)
+                    done()
+                })
+            })
+            after('should compare a given value with a given hash', function() {
+                expect(passingCompare).calledOnce
             })
         })
-        it('should compare a given value with a given hash', function() {
-            expect(compare).calledOnce
+        describe('when comparison fails', function() {
+            it('should call next with no error and false', function(done) {
+                bcryptSchema.verify(failingValue, hashValue, function(err, isVerified) {
+                    expect(err).to.not.exist
+                    expect(isVerified).to.equal(false)
+                    done()
+                })
+            })
+            after('should compare a given value with a given hash', function() {
+                expect(failingCompare).calledOnce
+            })
+        })
+        describe('when an error occurs in comparison', function() {
+            it('should call next with an error', function(done) {
+                bcryptSchema.verify(errorValue, hashValue, function(err, isVerified) {
+                    expect(err).to.exist
+                    done()
+                })
+            })
+            after('should compare a given value with a given hash', function() {
+                expect(errorCompare).calledOnce
+            })
         })
     })
     describe('set(value, hashField, next)', function() {
-        beforeEach('call set', function(done) {
-            var value = 'test-value',
-                hashField = 'hashedValue'
-            bcryptSchema.set(value, hashField, function() {
+        after('should hash a value', function() {
+            expect(passingGenSalt).calledTwice
+            expect(passingHash).calledOnce
+            expect(errorHash).calledOnce
+        })
+        it('should set the field with the generate hash', function(done) {
+            bcryptSchema.set(passingValue, hashField, function(err) {
+                expect(err).to.not.exist
+                expect(bcryptSchema[hashField]).to.equal(hashValue)
                 done()
             })
         })
-        it('should hash a value', function() {
-            expect(genSalt).calledOnce
-            expect(hash).calledOnce
-        })
-        it('should set the field with the generate hash', function() {
-            expect(bcryptSchema.hashedValue).to.equal('test-hash')
+        it('should call next with error when there is error in hash', function(done){
+            bcryptSchema.set(errorValue, hashField, function(err) {
+                expect(err).to.exist
+                done()
+            })
         })
     })
     describe('setEncyption(schema, options)', function() {
